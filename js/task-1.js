b9f7e4734899618a18081c28d4759495f21b7e29
illiakravchenko <illia.kravchenko@gmx.ch>
2024-03-16 17:49:27 +0100

    hw-js-7

--- js/task-1.js
+++ js/task-1.js
@@ -1,28 +1,11 @@
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-};
+const categoriesList = document.querySelector('#categories');
+const categoriesItems = categoriesList.querySelectorAll('.item');
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount());
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance());
-console.log(customer.getOrders());
+console.log(`Total categories: ${categoriesItems.length}`);
+
+categoriesItems.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const categoryItems = category.querySelectorAll('li');
+  console.log(`Category: ${categoryName}`);
+  console.log(`Number of items: ${categoryItems.length}`);
+});
